8d5f8a909fbc2fc8d5754a2b5f13b914b92899a3
onnttf <onnttf@gmail.com>
2024-03-15 16:38:59 +0800

    initial commit

---
+++ src/views/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div class="h-full flex justify-center items-center">
+        <div>
+            <h1 class="text-3xl font-bold underline">index</h1>
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
